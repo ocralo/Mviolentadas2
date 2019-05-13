8acc0bef534c801976b7c89910b211e8bea4d462
Juan Quintero <juan.seb.quintero@gmail.com>
2019-05-13 14:16:13 -0500

    Added google native login functionality.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { Firebase } from '@ionic-native/firebase/ngx';
-import { GooglePlus } from '@ionic-native/google-plus/ngx';
+import { GooglePlus } from "@ionic-native/google-plus/ngx";
 import { AngularFireModule } from "angularfire2";
 import { AngularFireAuth } from "angularfire2/auth";
 import { FIREBASE_CONFIG } from "./app.firebase.config";
-import { AlertController } from '@ionic/angular';
+import { AlertController } from "@ionic/angular";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
 
-import { CallNumber } from '@ionic-native/call-number/ngx';
+import { CallNumber } from "@ionic-native/call-number/ngx";
 
 @NgModule({
   declarations: [AppComponent],
@@ -34,7 +33,6 @@ import { CallNumber } from '@ionic-native/call-number/ngx';
     AngularFireModule,
     AngularFireAuth,
     AlertController,
-    Firebase,
     GooglePlus,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,67 +1,74 @@
-import { Component, OnInit } from '@angular/core';
-import { ToastController, NavController, AlertController } from '@ionic/angular';
-import { User } from '../modules/user';
+import { Component, OnInit } from "@angular/core";
+import {
+  ToastController,
+  NavController,
+  AlertController,
+  Platform
+} from "@ionic/angular";
+import { User } from "../modules/user";
 import { AngularFireAuth } from "angularfire2/auth";
 import { auth } from "firebase/app";
-
-
-
+import { GooglePlus } from "@ionic-native/google-plus/ngx";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"]
 })
 export class LoginPage implements OnInit {
-
-
   user = {} as User;
 
-
-  constructor(private afAuth: AngularFireAuth, public toastController: ToastController, public navCtrl: NavController, public alertController: AlertController) { }
-  async Login(user: User) {
+  constructor(
+    private afAuth: AngularFireAuth,
+    public toastController: ToastController,
+    public navCtrl: NavController,
+    public alertController: AlertController,
+    public gplus: GooglePlus,
+    public platform: Platform
+  ) {}
+  async Login() {
     if (this.user.email == null || this.user.password == null) {
       const alert = await this.alertController.create({
-        header: 'OH-OH',
-        subHeader: 'Datos incorrectos',
-        message: 'Al parecer no has ingresado ningun dato',
-        buttons: ['OK']
-
+        header: "OH-OH",
+        subHeader: "Datos incorrectos",
+        message: "Al parecer no has ingresado ningun dato",
+        buttons: ["OK"]
       });
       await alert.present();
     }
 
     try {
-      this.afAuth.auth.signInWithEmailAndPassword(this.user.email, this.user.password).then(async (res: any) => {
-        if (res) {
-          this.navCtrl.navigateRoot('/home');
-          const toast = await this.toastController.create({
-            message: "Bienvenido",
-            duration: 1000
-          });
-          toast.present();
-        }
-      })
+      this.afAuth.auth
+        .signInWithEmailAndPassword(this.user.email, this.user.password)
+        .then(async (res: any) => {
+          if (res) {
+            this.navCtrl.navigateRoot("/home");
+            const toast = await this.toastController.create({
+              message: "Bienvenido",
+              duration: 1000
+            });
+            toast.present();
+          }
+        })
         .catch(async (error: any) => {
           const alert = await this.alertController.create({
-            header: 'OH-OH',
-            subHeader: 'Datos incorrectos',
-            message: 'El usuario o la contraseña estan incorrectos',
-            buttons: ['OK']
+            header: "OH-OH",
+            subHeader: "Datos incorrectos",
+            message: "El usuario o la contraseña estan incorrectos",
+            buttons: ["OK"]
           });
           await alert.present();
           console.error(error);
         });
     } catch (e) {
       const alert = await this.alertController.create({
-        header: 'Oh-OH',
-        subHeader: 'Datos incorrectos',
-        message: 'El usuario o la contraseña estan incorrectos',
-        buttons: ['OK']
+        header: "Oh-OH",
+        subHeader: "Datos incorrectos",
+        message: "El usuario o la contraseña estan incorrectos",
+        buttons: ["OK"]
       });
       console.error(e);
     }
-
   }
   async Register() {
     const toast = await this.toastController.create({
@@ -72,31 +79,74 @@ export class LoginPage implements OnInit {
     //this.navCtrl.push('RegistrerPage');
   }
   LoginGoogle() {
-    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider()).then(async (res: any) => {
-      if (res) {
-        this.navCtrl.navigateRoot('/home');
-        const toast = await this.toastController.create({
-          message: "Bienvenido",
-          duration: 1000
-        });
-        toast.present();
-      }
-    })
+    this.afAuth.auth
+      .signInWithPopup(new auth.GoogleAuthProvider())
+      .then(async (res: any) => {
+        if (res) {
+          this.navCtrl.navigateRoot("/home");
+          const toast = await this.toastController.create({
+            message: "Bienvenido",
+            duration: 1000
+          });
+          toast.present();
+        }
+      })
       .catch(async (error: any) => {
         const alert = await this.alertController.create({
-          header: 'OH-OH',
-          subHeader: 'Datos incorrectos',
-          message: 'El usuario o la contraseña estan incorrectos',
-          buttons: ['OK']
+          header: "OH-OH",
+          subHeader: "Datos incorrectos",
+          message: "El usuario o la contraseña estan incorrectos",
+          buttons: ["OK"]
         });
         await alert.present();
         console.error(error);
-      });;
-
+      });
   }
 
-  ngOnInit() {
+  async nativeGoogleLogin() {
+    const gplusUser = await this.gplus.login({
+      webClientId:
+        "436211545918-rfcgvhu3t3574vnu7e3kgiai6hem801p.apps.googleusercontent.com",
+      offline: true,
+      scopes: "profile email"
+    });
+    this.afAuth.auth
+      .signInWithCredential(
+        auth.GoogleAuthProvider.credential(gplusUser.idToken)
+      )
+      .then(async (res: any) => {
+        if (res) {
+          this.navCtrl.navigateRoot("/home");
+          const toast = await this.toastController.create({
+            message: "Bienvenido",
+            duration: 1000
+          });
+          toast.present();
+        }
+      })
+      .catch(async (error: any) => {
+        const alert = await this.alertController.create({
+          header: "OH-OH",
+          subHeader: "Datos incorrectos",
+          message: "El usuario o la contraseña estan incorrectos",
+          buttons: ["OK"]
+        });
+        await alert.present();
+        console.error(error);
+      });
+  }
+
+  googleLogin() {
+    if (this.platform.is("cordova")) {
+      this.nativeGoogleLogin();
+    } else {
+      this.LoginGoogle();
+    }
+  }
+
+  signOut() {
     this.afAuth.auth.signOut();
   }
 
+  ngOnInit() {}
 }
